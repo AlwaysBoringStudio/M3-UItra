7135e0591472621a82664741701751b501b541af
AlwaysBoringStudio <104271144+AlwaysBoringStudio@users.noreply.github.com>
2022-08-23 23:40:22 +0800

    update

--- M3 UItra/Other func/barchartView.swift
+++ M3 UItra/Other func/barchartView.swift
@@ -14,50 +14,96 @@ struct barchartView: View {
     @State var small = Float(10)
     @State var text = false
     var body: some View {
-        HStack {
-            VStack {
+        if data > 1500 {
+            HStack {
                 VStack {
-                    ZStack {
-                        VStack {
-                            Spacer()
-                            Rectangle()
-                                .foregroundColor(.green)
-                                .opacity(0)
-                                .frame(width: 20*CGFloat(size), height: CGFloat(per)/CGFloat(small))
-                        }
+                    VStack {
                         ZStack {
                             VStack {
                                 Spacer()
                                 Rectangle()
-                                    .frame(width: 20*CGFloat(size), height: CGFloat(data)/CGFloat(small))
+                                    .foregroundColor(.green)
+                                    .opacity(0)
+                                    .frame(width: 20*CGFloat(size), height: CGFloat(per)/CGFloat(small))
                             }
-                            if text == true {
+                            ZStack {
                                 VStack {
                                     Spacer()
-                                    Text("今")
-                                        .foregroundColor(.blue)
-                                        .padding(.top)
-                                    Text("天")
-                                        .foregroundColor(.blue)
-                                        .padding(.bottom)
+                                    Rectangle()
+                                        .foregroundColor(.red)
+                                        .frame(width: 20*CGFloat(size), height: CGFloat(data)/CGFloat(small))
+                                }
+                                if text == true {
+                                    VStack {
+                                        Spacer()
+                                        Text("今")
+                                            .foregroundColor(.blue)
+                                            .padding(.top)
+                                        Text("天")
+                                            .foregroundColor(.blue)
+                                            .padding(.bottom)
+                                    }
                                 }
                             }
                         }
+                        
                     }
-                    
+                    .frame(height: CGFloat(per)/CGFloat(small))
+                    Text("\(Int(data))")
                 }
-                .frame(height: CGFloat(per)/CGFloat(small))
-                Text("\(Int(data))")
             }
+            .padding(3)
+        } else {
+            HStack {
+                VStack {
+                    VStack {
+                        ZStack {
+                            VStack {
+                                Spacer()
+                                Rectangle()
+                                    .foregroundColor(.green)
+                                    .opacity(0)
+                                    .frame(width: 20*CGFloat(size), height: CGFloat(per)/CGFloat(small))
+                            }
+                            ZStack {
+                                VStack {
+                                    Spacer()
+                                    Rectangle()
+                                        .frame(width: 20*CGFloat(size), height: CGFloat(data)/CGFloat(small))
+                                }
+                                if text == true {
+                                    VStack {
+                                        Spacer()
+                                        Text("今")
+                                            .foregroundColor(.blue)
+                                            .padding(.top)
+                                        Text("天")
+                                            .foregroundColor(.blue)
+                                            .padding(.bottom)
+                                    }
+                                }
+                            }
+                        }
+                        
+                    }
+                    .frame(height: CGFloat(per)/CGFloat(small))
+                    Text("\(Int(data))")
+                }
+            }
+            .padding(3)
         }
-        .padding(3)
     }
 }
 
 struct barchartView_Previews: PreviewProvider {
     static var previews: some View {
-        ZStack {
-            barchartView(data: 1000, per: 2000, text: true)
+        HStack {
+            barchartView(data: 200, per: 2000, text: true)
+            barchartView(data: 500, per: 2000, text: true)
+            barchartView(data: 900, per: 2000, text: true)
+            barchartView(data: 1200, per: 2000, text: true)
+            barchartView(data: 1600, per: 2000, text: true)
+            barchartView(data: 2000, per: 2000, text: true)
         }
     }
 }
